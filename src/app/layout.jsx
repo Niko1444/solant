bbb6c70fd46bc9c36c5dcdd7b7089b31001e306c
Le Tien Phat <91601109+Niko1444@users.noreply.github.com>
2024-06-15 11:26:43 +0700

    clean the code

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,43 +1,46 @@
-'use client';
+'use client'
 
 import './globals.css'
 import '@fontsource/inika'
 import '@fontsource-variable/yrsa'
 
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react'
 
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import {
+	ConnectionProvider,
+	WalletProvider,
+} from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
 
-import "@solana/wallet-adapter-react-ui/styles.css"
+import '@solana/wallet-adapter-react-ui/styles.css'
 
-// a server component so It will be temporarily disabled
+// Comment out the server-side metadata as it's temporarily disabled
 // export const metadata = {
 // 	title: 'Solant',
 // 	description: 'The productive app for studying and working',
 // }
 
 export default function RootLayout({ children }) {
+	const [mounted, setMounted] = useState(false)
 
-    const [mounted, setMounted] = useState(false)
+	const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
 
-    const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
+	useEffect(() => {
+		setMounted(true)
+	}, [])
 
-    useEffect(() => {
-        setMounted(true)
-    }, []);
-
-	// no background please fix <3
-    return (
-        mounted && 
-        <ConnectionProvider endpoint="https://solana-devnet.g.alchemy.com/v2/Yw6g1yw54yXlyLYD5kXbxH4-8tAxvVGY" config={{commitment: "confirmed"}}>
-            <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>
-					{mounted && children}
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    )
+	return (
+		mounted && (
+			<ConnectionProvider
+				endpoint="https://solana-devnet.g.alchemy.com/v2/Yw6g1yw54yXlyLYD5kXbxH4-8tAxvVGY"
+				config={{ commitment: 'confirmed' }}
+			>
+				<WalletProvider wallets={wallets} autoConnect>
+					<WalletModalProvider>{children}</WalletModalProvider>
+				</WalletProvider>
+			</ConnectionProvider>
+		)
+	)
 }
--- src/components/ui/candyMint.jsx
+++ src/components/ui/candyMint.jsx
@@ -1,88 +1,98 @@
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { useCallback, useMemo } from 'react';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { useCallback, useMemo } from 'react'
 // import useUserSOLBalanceStore from '../../nft-build/useUserSOLBalanceStore';
-import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
-import { generateSigner, transactionBuilder, publicKey, some } from '@metaplex-foundation/umi';
-import { fetchCandyMachine, mintV2, mplCandyMachine, safeFetchCandyGuard } from "@metaplex-foundation/mpl-candy-machine";
-import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters';
-import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata';
-import { setComputeUnitLimit } from '@metaplex-foundation/mpl-toolbox';
-import { clusterApiUrl } from '@solana/web3.js';
-import * as bs58 from 'bs58';
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
+import {
+	generateSigner,
+	transactionBuilder,
+	publicKey,
+	some,
+} from '@metaplex-foundation/umi'
+import {
+	fetchCandyMachine,
+	mintV2,
+	mplCandyMachine,
+	safeFetchCandyGuard,
+} from '@metaplex-foundation/mpl-candy-machine'
+import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters'
+import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata'
+import { setComputeUnitLimit } from '@metaplex-foundation/mpl-toolbox'
+import { clusterApiUrl } from '@solana/web3.js'
+import * as bs58 from 'bs58'
 
-const quicknodeEndpoint = "https://solana-devnet.g.alchemy.com/v2/Yw6g1yw54yXlyLYD5kXbxH4-8tAxvVGY " || clusterApiUrl('devnet');
-const candyMachineAddress = publicKey("D6EKAouiQNwxHznYPUf13e4qiC1ckaMLtuKRpypgvLy1");
-const treasury = publicKey("H6oEWNDbXCCXXwjZG4XViVTdctoXubnGa1LDQ76vWNk5");
+const quicknodeEndpoint =
+	'https://solana-devnet.g.alchemy.com/v2/Yw6g1yw54yXlyLYD5kXbxH4-8tAxvVGY ' ||
+	clusterApiUrl('devnet')
+const candyMachineAddress = publicKey(
+	'D6EKAouiQNwxHznYPUf13e4qiC1ckaMLtuKRpypgvLy1',
+)
+const treasury = publicKey('H6oEWNDbXCCXXwjZG4XViVTdctoXubnGa1LDQ76vWNk5')
 
 export default function CandyMint() {
-    const { connection } = useConnection();
-    const wallet = useWallet();
-    // const { getUserSOLBalance } = useUserSOLBalanceStore();
+	const { connection } = useConnection()
+	const wallet = useWallet()
+	// const { getUserSOLBalance } = useUserSOLBalanceStore();
 
-    const umi = useMemo(() =>
-        createUmi(quicknodeEndpoint)
-            .use(walletAdapterIdentity(wallet))
-            .use(mplCandyMachine())
-            .use(mplTokenMetadata()),
-        [wallet, mplCandyMachine, walletAdapterIdentity, mplTokenMetadata, quicknodeEndpoint, createUmi]
-    );
+	const umi = useMemo(
+		() =>
+			createUmi(quicknodeEndpoint)
+				.use(walletAdapterIdentity(wallet))
+				.use(mplCandyMachine())
+				.use(mplTokenMetadata()),
+		[wallet],
+	)
 
-    const onClick = useCallback(async () => {
-        if (!wallet.publicKey) {
-            console.log('error', 'Wallet not connected!');
-            return;
-        }
+	const onClick = useCallback(async () => {
+		if (!wallet.publicKey) {
+			console.log('error', 'Wallet not connected!')
+			return
+		}
 
-        const candyMachine = await fetchCandyMachine(
-            umi,
-            candyMachineAddress,
-        );
-        const candyGuard = await safeFetchCandyGuard(
-            umi,
-            candyMachine.mintAuthority,
-        );
-        try {
-            const nftMint = generateSigner(umi);
-            const transaction = await transactionBuilder()
-                .add(setComputeUnitLimit(umi, { units: 800_000 }))
-                .add(
-                    mintV2(umi, {
-                        candyMachine: candyMachine.publicKey,
-                        candyGuard: candyGuard?.publicKey,
-                        nftMint,
-                        collectionMint: candyMachine.collectionMint,
-                        collectionUpdateAuthority: candyMachine.authority,
-                        mintArgs: {
-                            solPayment: some({ destination: treasury }),
-                        },
-                    })
-                );
-            const { signature } = await transaction.sendAndConfirm(umi, {
-                confirm: { commitment: "confirmed" },
-            });
-            const txid = bs58.encode(signature);
-            console.log('success', `Mint successful! ${txid}`)
+		const candyMachine = await fetchCandyMachine(umi, candyMachineAddress)
+		const candyGuard = await safeFetchCandyGuard(
+			umi,
+			candyMachine.mintAuthority,
+		)
+		try {
+			const nftMint = generateSigner(umi)
+			const transaction = transactionBuilder()
+				.add(setComputeUnitLimit(umi, { units: 800_000 }))
+				.add(
+					mintV2(umi, {
+						candyMachine: candyMachine.publicKey,
+						candyGuard: candyGuard?.publicKey,
+						nftMint,
+						collectionMint: candyMachine.collectionMint,
+						collectionUpdateAuthority: candyMachine.authority,
+						mintArgs: {
+							solPayment: some({ destination: treasury }),
+						},
+					}),
+				)
+			const { signature } = await transaction.sendAndConfirm(umi, {
+				confirm: { commitment: 'confirmed' },
+			})
+			const txid = bs58.encode(signature)
+			console.log('success', `Mint successful! ${txid}`)
 
-            // getUserSOLBalance(wallet.publicKey, connection);
-        } catch (error) {
-            console.log('error', `Mint failed! ${error?.message}`);
-        }
-    // }, [wallet, connection, getUserSOLBalance, umi, candyMachineAddress, treasury]);
+			// getUserSOLBalance(wallet.publicKey, connection);
+		} catch (error) {
+			console.log('error', `Mint failed! ${error?.message}`)
+		}
+		// }, [wallet, connection, getUserSOLBalance, umi, candyMachineAddress, treasury]);
+	}, [wallet, umi])
 
-    }, [wallet, connection, umi, candyMachineAddress, treasury]);
-
-    return (
-        <div className="flex flex-row justify-center">
-            <div className="relative group items-center">
-                <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
-                    rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-                <button
-                    className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
-                    onClick={onClick}
-                >
-                    <span>Mint NFT </span>
-                </button>
-            </div>
-        </div>
-    );
-};
+	return (
+		<div className="flex flex-row justify-center">
+			<div className="group relative items-center">
+				<div className="animate-tilt absolute -inset-0.5 m-1 rounded-lg bg-gradient-to-r from-indigo-500 to-fuchsia-500 opacity-20 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+				<button
+					className="btn m-2 animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 px-8 text-black hover:from-white hover:to-purple-300"
+					onClick={onClick}
+				>
+					<span>Mint NFT </span>
+				</button>
+			</div>
+		</div>
+	)
+}
