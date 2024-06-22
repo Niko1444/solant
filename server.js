57f39e0f036e759b1a5a601e8a3b9ce8234f4160
meomun1 <maluong1504@gmail.com>
2024-06-22 18:25:54 +0700

    try to fix deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { Server } from 'socket.io'
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = process.env.HOSTNAME || 'localhost'
 const port = process.env.PORT || 3000
-const app = next({ dev, hostname, port })
+const app = next({ dev })
 const handler = app.getRequestHandler()
 
 app.prepare().then(() => {
